36eae5a0097fe01f2de89a21639c7c7500158823
Farhan Hasin Saad <farhan.hasin09@northsouth.edu>
2020-09-15 22:09:44 +0600

    Designed Login Page

---
+++ login.php
@@ -0,0 +1,29 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel = "stylesheet" href = "login.css" />
+<div class="sidenav">
+         <div class="login-main-text">
+            <p class="zoom"style="font-size:100px">Uber</p><p class="zoom" style="font-size:60px">Insider </p> <br>
+            <p class="zoom2"> Uber Employee's Database Management Web Application</p><br>
+         </div>
+      </div>
+      <div class="main">
+         <div class="col-md-6 col-sm-12">
+            <div class="login-form">
+               <form>
+                  <div class="form-group">
+                     <label>User Name</label>
+                     <input type="text" class="form-control" placeholder="User Name">
+                  </div>
+                  <div class="form-group">
+                     <label>Password</label>
+                     <input type="password" class="form-control" placeholder="Password">
+                  </div>
+                  <button type="submit" class="btn btn-black">Login</button>
+                  <p style="padding-top:50%"> Visit our official website <a href="https://www.uber.com/bd/en/" target="_blank">here </a> </p>
+               </form>
+            </div>
+         </div>
+      </div>
+    
