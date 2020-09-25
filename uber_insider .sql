f5c208f38e29b2c0cf1a52a2d0f3717387aa35e1
Raisa Nusrat Chowdhury <71133154+raisanusrat222@users.noreply.github.com>
2020-09-25 21:54:11 +0600

    Database updated

--- uber_insider .sql
+++ uber_insider .sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 23, 2020 at 06:26 PM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.2.31
+-- Generation Time: Sep 25, 2020 at 05:51 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,9 +40,7 @@ CREATE TABLE `car` (
 --
 
 INSERT INTO `car` (`CAR_LICENSE_NO`, `CAR_NAME`, `CAR_COLOR`, `OWNER_ID`, `DRIVER_ID`) VALUES
-('Dhaka metro-GA 15-4919', 'Toyota Corolla', 'Black', 2, 101),
-('Dhaka metro-GA 20-0016', 'Toyota Corolla', 'Black', 1, 102),
-('Dhaka metro-GA 21-8571', 'Toyota Noah', 'White', 2, 101);
+('Dhaka metro-GA 20-0016', 'Toyota Corolla', 'Black', 1, 102);
 
 -- --------------------------------------------------------
 
@@ -63,7 +61,7 @@ CREATE TABLE `complain_box` (
 --
 
 CREATE TABLE `driver` (
-  `DRIVER_ID` int(10) NOT NULL,
+  `DRIVER_ID` int(11) NOT NULL,
   `NAME` varchar(25) NOT NULL,
   `DRIVER_ADDRESS` varchar(70) DEFAULT NULL,
   `PHONE_NO` varchar(20) NOT NULL,
@@ -81,7 +79,6 @@ CREATE TABLE `driver` (
 --
 
 INSERT INTO `driver` (`DRIVER_ID`, `NAME`, `DRIVER_ADDRESS`, `PHONE_NO`, `DRIVEN_CAR_NO`, `HIRE_DATE`, `MONTHLY_EARNING`, `UBER_CONTRIBUTION`, `RIDE_NO`, `RATING`, `OWNER_ID`) VALUES
-(101, 'Saad Pasha', '32/A south mughdapara,Dhaka', '01921384671', 'Dhaka metro-GA 15-4919', '2017-07-02', 12000.75, 3000.18, 40, 4.50, 2),
 (102, 'Anim Hasan', '12/A south jatrabari,dhaka', '01872230823', 'Dhaka metro-GA 20-0016', '2018-05-03', 10000.50, 2500.12, 35, 4.00, 1);
 
 -- --------------------------------------------------------
@@ -101,6 +98,7 @@ CREATE TABLE `log_in` (
 --
 
 INSERT INTO `log_in` (`id`, `username`, `password`) VALUES
+(3, 'raisan', '$2y$10$MPrHIj0BxZpJQGIU8ub9L.RErh0/BFUaD2AHwKJAolC8pxkTC2Lwe'),
 (2, 'saadism7', '$2y$10$rwfsznzFUTyV9poSeMKn9ORwwNxgJyGFVM95Nn0.Sh6hEdqNE8YzC'),
 (1, 'test', '$2y$10$.POWRtCUkuU6mv4j5rDAW.PGmBH5Jcf4YYB0TDJsnAItxWzfJuqa2');
 
@@ -111,7 +109,7 @@ INSERT INTO `log_in` (`id`, `username`, `password`) VALUES
 --
 
 CREATE TABLE `owner` (
-  `OWNER_ID` int(15) NOT NULL,
+  `OWNER_ID` int(11) NOT NULL,
   `NAME` varchar(20) DEFAULT NULL,
   `ADDRESS` varchar(70) DEFAULT NULL,
   `PHONE_NO` varchar(30) DEFAULT NULL,
@@ -171,10 +169,22 @@ ALTER TABLE `owner`
 --
 
 --
+-- AUTO_INCREMENT for table `driver`
+--
+ALTER TABLE `driver`
+  MODIFY `DRIVER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+
+--
 -- AUTO_INCREMENT for table `log_in`
 --
 ALTER TABLE `log_in`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `owner`
+--
+ALTER TABLE `owner`
+  MODIFY `OWNER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -184,8 +194,8 @@ ALTER TABLE `log_in`
 -- Constraints for table `car`
 --
 ALTER TABLE `car`
-  ADD CONSTRAINT `car_ibfk_1` FOREIGN KEY (`OWNER_ID`) REFERENCES `owner` (`OWNER_ID`) ON DELETE CASCADE,
-  ADD CONSTRAINT `car_ibfk_2` FOREIGN KEY (`DRIVER_ID`) REFERENCES `driver` (`DRIVER_ID`) ON DELETE CASCADE;
+  ADD CONSTRAINT `car_ibfk_2` FOREIGN KEY (`DRIVER_ID`) REFERENCES `driver` (`DRIVER_ID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `car_ibfk_3` FOREIGN KEY (`OWNER_ID`) REFERENCES `owner` (`OWNER_ID`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `driver`
